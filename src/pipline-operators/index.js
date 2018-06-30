d638f5bc8f285597e4207067f9fb048a37533308
a.gusejnov <a.gusejnov@ddemo.ru>
2018-06-30 10:19:43 +0300

    pipline-opetators

---
+++ src/pipline-operators/index.js
@@ -0,0 +1,11 @@
+export function doubleSay (str) {
+    return str + ", " + str;
+}
+
+export function capitalize (str) {
+    return str[0].toUpperCase() + str.substring(1);
+}
+
+export function exclaim (str) {
+    return str + '!';
+}
---
+++ src/pipline-operators/pipline-operators.test.js
@@ -0,0 +1,28 @@
+import test from 'ava';
+import { doubleSay, capitalize, exclaim } from './index';
+
+test('Simple pipeline usage', (t) => {
+    let result = "hello"
+        |> doubleSay
+        |> capitalize
+        |> exclaim;
+
+    t.is(result, 'Hello, hello!');
+});
+
+test('Partial application pipeline', (t) => {
+    let result = -5
+        |> (_ => Math.max(0, _));
+
+    t.is(result, 0);
+});
+
+test('Async pipeline', async (t) => {
+    const asyncAdd = (number) => Promise.resolve(number + 5);
+    const subtractOne = (num1) => num1 - 1;
+    const result = 10
+        |> asyncAdd
+        |> (async (num) => subtractOne(await num));
+
+    t.is(await result, 14);
+});
